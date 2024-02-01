c28e3b05e87c94d36fdfb7a5144ef761542062f6
Ivan Ferrera <ivanferrera@gmail.com>
2024-02-01 17:39:29 -0500

    use existing redis caching middleware

--- server/api/routes/locationRoutes.ts
+++ server/api/routes/locationRoutes.ts
@@ -3,53 +3,46 @@
 import { type Request, type Response, Router, type NextFunction } from 'express'
 import { countries, country, provinces, province, municipalities, municipality, sectors, sector } from '@api/controllers/locationsController'
 import { cacheMiddleware } from '@middleware/cacheMiddleware'
-import { cacheStore, type CustomStore } from '@utils/redis'
 
 const locationRoutes: Router = Router()
 
-const asyncCacheMiddleware = (cacheStore: CustomStore) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    cacheMiddleware(cacheStore)(req, res, next).catch(next)
-  }
-}
-
 // List all countries
-locationRoutes.get('/countries', asyncCacheMiddleware(cacheStore), (req, res, next) => {
+locationRoutes.get('/countries', cacheMiddleware, (req: Request, res: Response, next: NextFunction) => {
   countries(req, res).catch(next)
 })
 
 // Get single country by id
-locationRoutes.get('/countries/:id', asyncCacheMiddleware(cacheStore), (req, res, next) => {
+locationRoutes.get('/countries/:id', cacheMiddleware, (req: Request, res: Response, next: NextFunction) => {
   country(req, res).catch(next)
 })
 
 // List all provinces by country_id
-locationRoutes.get('/provinces', asyncCacheMiddleware(cacheStore), (req, res, next) => {
+locationRoutes.get('/provinces', cacheMiddleware, (req: Request, res: Response, next: NextFunction) => {
   provinces(req, res).catch(next)
 })
 
 // Get a single province by id
-locationRoutes.get('/provinces/:id', asyncCacheMiddleware(cacheStore), (req, res, next) => {
+locationRoutes.get('/provinces/:id', cacheMiddleware, (req: Request, res: Response, next: NextFunction) => {
   province(req, res).catch(next)
 })
 
 // List all municipalities by province_id
-locationRoutes.get('/municipalities', asyncCacheMiddleware(cacheStore), (req, res, next) => {
+locationRoutes.get('/municipalities', cacheMiddleware, (req: Request, res: Response, next: NextFunction) => {
   municipalities(req, res).catch(next)
 })
 
 // Get a single municipality by id
-locationRoutes.get('/municipalities/:id', asyncCacheMiddleware(cacheStore), (req, res, next) => {
+locationRoutes.get('/municipalities/:id', cacheMiddleware, (req: Request, res: Response, next: NextFunction) => {
   municipality(req, res).catch(next)
 })
 
 // List all sectors by municipality_id
-locationRoutes.get('/sectors', asyncCacheMiddleware(cacheStore), (req, res, next) => {
+locationRoutes.get('/sectors', cacheMiddleware, (req: Request, res: Response, next: NextFunction) => {
   sectors(req, res).catch(next)
 })
 
 // Get a single sector by id
-locationRoutes.get('/sectors/:id', asyncCacheMiddleware(cacheStore), (req, res, next) => {
+locationRoutes.get('/sectors/:id', cacheMiddleware, (req: Request, res: Response, next: NextFunction) => {
   sector(req, res).catch(next)
 })
 
