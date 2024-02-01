e76dacb5459350d214edf8224842de0df76950a3
Ivan Ferrera <ivanferrera@gmail.com>
2024-02-01 17:30:58 -0500

    use aliases

--- server/api/routes/locationRoutes.ts
+++ server/api/routes/locationRoutes.ts
@@ -1,9 +1,9 @@
 // server/api/listings/routes/locationRoutes.ts
 
 import { type Request, type Response, Router, type NextFunction } from 'express'
-import { countries, country, provinces, province, municipalities, municipality, sectors, sector } from '../controllers/locationsController'
-import { cacheMiddleware } from '../../../middleware/cacheMiddleware'
-import { cacheStore, type CustomStore } from '../../../utils/redis'
+import { countries, country, provinces, province, municipalities, municipality, sectors, sector } from '@api/controllers/locationsController'
+import { cacheMiddleware } from '@middleware/cacheMiddleware'
+import { cacheStore, type CustomStore } from '@utils/redis'
 
 const locationRoutes: Router = Router()
 
