5d03d53b8790c085c1938c6fc6b755c41db290e9
Ivan Ferrera <ivanferrera@gmail.com>
2024-02-15 17:50:27 -0500

    initial commit

---
+++ server/api/models/imageModel.ts
@@ -0,0 +1,75 @@
+// server/api/models/imageModel.ts
+
+import {
+  Model,
+  DataTypes,
+  type Sequelize,
+  type InferAttributes,
+  type InferCreationAttributes,
+  type BelongsToGetAssociationMixin,
+  type BelongsToSetAssociationMixin,
+  type BelongsToCreateAssociationMixin,
+  type CreationOptional
+} from 'sequelize'
+import type Property from './propertyModel'
+
+class Image extends Model<InferAttributes<Image>, InferCreationAttributes<Image>> {
+  declare id: CreationOptional<string>
+  declare propertyId: string
+  declare url: string
+  declare description: string | null
+
+  // timestamps!
+  declare readonly createdAt?: CreationOptional<Date>
+  declare readonly updatedAt?: CreationOptional<Date>
+
+  // Associations
+  declare getProperty: BelongsToGetAssociationMixin<Property>
+  declare setProperty: BelongsToSetAssociationMixin<Property, string>
+  declare createProperty: BelongsToCreateAssociationMixin<Property>
+
+  public static associate (models: {
+    Property: typeof Property
+  }): void {
+    Image.belongsTo(models.Property, { foreignKey: 'propertyId', as: 'property' })
+  }
+}
+
+export const initImage = (sequelize: Sequelize): typeof Image => {
+  Image.init({
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false,
+      unique: true
+    },
+    propertyId: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      references: {
+        model: 'Properties',
+        key: 'id'
+      },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true
+    },
+    createdAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE
+  }, {
+    sequelize,
+    modelName: 'Image'
+  })
+
+  return Image
+}
+
+export default Image
