6a03253468b02aba59d413edd93fe9374bd8d9ef
Ivan Ferrera <ivanferrera@gmail.com>
2024-03-28 10:43:48 -0400

    add sentry init and cors middleware refactor

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,6 @@
 // Ensure environment is configured before importing any other modules
 import '@utils/configureEnvironment'
 
-import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import express from 'express'
 import listingDB from './listingDB'
@@ -14,36 +13,13 @@ import customPoweredBy from './middleware/customPoweredBy'
 import { consoleLogging } from './middleware/consoleLogging'
 import { errorHandling } from './middleware/errorHandling'
 import { handle404 } from './middleware/handle404'
-
-// CORS options configuration
-const corsOptions = {
-  // Function to dynamically set allowed origins based on incoming request
-  origin: (
-    origin: string | undefined,
-    callback: (error: Error | null, allow?: boolean) => void
-  ) => {
-    // Retrieve list of allowed domains from an environment variable
-    const allowedDomains = getRequiredEnvVariable('ALLOWED_ORIGINS').split(',')
-
-    if (process.env.NODE_ENV !== 'production') {
-      // Allow all origins in non-production environments
-      console.log(`\x1b[32mAllowed origin (non-production): ${origin}\x1b[0m`)
-      callback(null, true)
-    } else if (origin !== undefined && allowedDomains.includes(origin)) {
-      // In production, check against the list of allowed origins
-      console.log(`\x1b[32mAllowed origin: ${origin}\x1b[0m`)
-      callback(null, true)
-    } else {
-      // Block origins not in the allowed list
-      console.log(`\x1b[31mBlocked origin: ${origin}\x1b[0m`)
-      callback(new Error('403: Origin not permitted by CORS policy'), false)
-    }
-  },
-  // Enable credentials (cookies) for CORS requests
-  credentials: true
-}
+import { initSentry, sentryErrorHandler } from '@utils/sentry'
+import { corsMiddleware } from './middleware/corsMiddleware'
 
 const server = express()
+
+// Initialize Sentry
+initSentry(server)
 
 // Console logging middleware
 server.use(consoleLogging)
@@ -51,11 +27,8 @@ server.use(consoleLogging)
 // server.disable('x-powered-by');
 server.use(customPoweredBy(getRequiredEnvVariable('POWERED_BY')))
 
-// Apply CORS middleware with custom options
-server.use(cors(corsOptions))
-
-// Handle preflight requests (OPTIONS)
-server.options('*', cors(corsOptions))
+// Apply custom CORS middleware
+server.use(corsMiddleware)
 
 // Parse incoming JSON payloads
 server.use(express.json())
@@ -90,6 +63,9 @@ server.listen(port, () => {
   const errorMessage = `\x1b[31mFailed to start server: ${err.message}\x1b[0m` // Red text
   console.error(errorMessage)
 })
+
+// Use Sentry's error handler
+server.use(sentryErrorHandler())
 
 // Error handling middleware
 server.use(errorHandling)
