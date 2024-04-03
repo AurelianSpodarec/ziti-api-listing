af05580ae59d4869606276f92f19e212294002a9
Ivan Ferrera <ivanferrera@gmail.com>
2024-04-03 14:12:14 -0400

    new schema

--- scripts/postgres/seed-property-types.sql
+++ scripts/postgres/seed-property-types.sql
@@ -1,4 +1,14 @@
-INSERT INTO "PropertyTypes" ("land", "landDetails", "apartment", "apartmentDetails", "house", "houseDetails", "createdAt", "updatedAt") VALUES
-(true, '{"size": "500 sqm", "location": "rural"}', false, NULL, false, NULL, NOW(), NOW()),
-(false, NULL, true, '{"floors": 2, "amenities": ["pool", "gym"]}', false, NULL, NOW(), NOW()),
-(false, NULL, false, NULL, true, '{"size": "300 sqm", "garage": true}', NOW(), NOW());
+INSERT INTO "PropertyTypes" ("name", "createdAt", "updatedAt") VALUES
+('Land', NOW(), NOW()),
+('Apartment', NOW(), NOW()),
+('House', NOW(), NOW());
+
+
+
+
+
+
+-- INSERT INTO "PropertyTypes" ("land", "landDetails", "apartment", "apartmentDetails", "house", "houseDetails", "createdAt", "updatedAt") VALUES
+-- (true, '{"size": "500 sqm", "location": "rural"}', false, NULL, false, NULL, NOW(), NOW()),
+-- (false, NULL, true, '{"floors": 2, "amenities": ["pool", "gym"]}', false, NULL, NOW(), NOW()),
+-- (false, NULL, false, NULL, true, '{"size": "300 sqm", "garage": true}', NOW(), NOW());
